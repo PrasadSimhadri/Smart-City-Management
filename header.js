bcbcf134ac0ea296e464f539aad45952ff3c3611
PrasadSimhadri <saisimhadri2207@gmail.com>
2025-02-24 13:37:26 +0530

    deleted old files of transport

--- header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-container").innerHTML = data;
-            console.log("header.js loaded and executed");
-        })
-        .catch(error => console.log("Error loading header:", error));
-});
