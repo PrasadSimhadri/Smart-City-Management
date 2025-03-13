ad7892d7249c01da83a61fbd76100d16c93060fe
PrasadSimhadri <saisimhadri2207@gmail.com>
2025-03-13 21:03:07 +0530

    changed main redirect and removed few cards in sub main pages

--- index.js
+++ index.js
@@ -839,5 +839,5 @@ app.get("/api/search_multimodal", async (req, res) => {
 
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:3000/home");
 });
